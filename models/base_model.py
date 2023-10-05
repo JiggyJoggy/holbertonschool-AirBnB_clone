ba1c8dd58a8e6dc6852935f82ce901343c85db60
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-05 16:16:32 -0500

    base_model.py added to_dict keys/values

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ class BaseModel():
     """Defines all common attributes/methods for other classes"""
     def __init__(self, ):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+        self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
@@ -17,8 +17,13 @@ class BaseModel():
 
     def save(self):
         """Updates updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
     
     def to_dict(self):
         """Returns a dictionary containing all keys/values"""
-        
+        dictionary = self.__dict__.copy()
+        dictionary.update({
+            'created_at': datetime.isoformat(self.created_at),
+            '__class__': self.__class__.__name__,
+            'updated_at': datetime.isoformat(self.updated_at)
+        })
