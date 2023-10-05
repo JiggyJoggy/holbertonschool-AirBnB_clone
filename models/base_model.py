7a1ce6158efc02b427646aa71e3351fac15a2190
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-05 16:33:53 -0500

    cleaning up __init__

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ import datetime
 
 class BaseModel():
     """Defines all common attributes/methods for other classes"""
-    def __init__(self, ):
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
