c1257a4fd96d34fd5ca206d6f7b5023769c586f3
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-06 16:17:04 -0500

    console updated with EOF and quit commands

--- console.py
+++ console.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Console module"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """The command line"""
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        EOF command to exit the program
+        """
+        return True
+    
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
