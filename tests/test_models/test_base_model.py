1f59b3fa812640aff5e5573ec482d013c1f75850
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-06 15:15:23 -0500

    Setting up unittesting

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 """Unittest classes"""
 import unittest
+from models.base_model import BaseModel
 
 
+class TestBaseModel(unittest.TestCase):
+    """Instances method tests"""
+
+
+class TestBaseModel_str(unittest.TestCase):
+    """__str__ tests"""
+
+
+class TestBaseModel_to_dict(unittest.TestCase):
+    """Dictionary tests"""
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
