48f7c56c00a790b2d77d1aff98ba01e5ea49d75b
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-06 17:30:34 -0500

    Console updates

--- console.py
+++ console.py
@@ -5,23 +5,25 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class for the console using cmd"""
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """
         Quit command to exit the program
         """
+        print("Exiting...")
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """
         EOF command to exit the program (Ctrl+D)
         """
+        print("Exiting...")
         return True
 
     def emptyline(self):
         """Print nothing if line is empty"""
-        return False
+        pass
 
 
 if __name__ == '__main__':
