6e4577c7a67405fa73014a4806a4215589e703a6
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-08 11:42:42 -0500

    Added *args and **kwargs into BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,18 @@ from datetime import datetime
 
 class BaseModel():
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs and kwargs > 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation"""
