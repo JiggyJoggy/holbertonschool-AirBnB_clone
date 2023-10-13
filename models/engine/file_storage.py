e24ed6c31ddc9a123c41b63d98276d72491dbe9b
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-13 13:01:02 -0500

    Updated file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""File storage module"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    """File storage class"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+    
+    def new(self, obj):
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+    
+    def save(self):
+        """Placeholder"""
+        pass
